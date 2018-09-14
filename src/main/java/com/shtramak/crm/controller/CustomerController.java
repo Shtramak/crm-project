909213e0cc2eae2962b70bf6c0e2c5533fd26f18
shtramak <shtramak@gmail.com>
2018-09-14 13:42:45 +0300

    update CustomerController listCustomers method, inject CustomerDao

--- src/main/java/com/shtramak/crm/controller/CustomerController.java
+++ src/main/java/com/shtramak/crm/controller/CustomerController.java
@@ -1,5 +1,10 @@
 package com.shtramak.crm.controller;
 
+import java.util.List;
+
+import com.shtramak.crm.dao.CustomerDao;
+import com.shtramak.crm.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customer")
 public class CustomerController {
 
-    @RequestMapping("/list")
-    public String listCustomers(Model model){
+    CustomerDao customerDao;
 
+    @Autowired
+    public CustomerController(CustomerDao customerDao) {
+        this.customerDao = customerDao;
+    }
+
+    @RequestMapping("/list")
+    public String listCustomers(Model model) {
+        List<Customer> customers = customerDao.getCustomers();
+        model.addAttribute("customers", customers);
         return "list-customers";
     }
 }
