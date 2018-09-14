4c0153038a5e96fa06cbf8aaa6e8dcb6cf4662dd
shtramak <shtramak@gmail.com>
2018-09-14 10:49:24 +0300

    add Customer entity and lombok dependency
    rename appSessionFactory to sessionFactory in spring-mvc-config file

--- src/main/resources/sql/crateCustomerTable.sql
+++ src/main/resources/sql/crateCustomerTable.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS customer;
 
 CREATE TABLE customer(
   id SERIAL PRIMARY KEY,
-  firs_name TEXT,
+  first_name TEXT,
   last_name TEXT,
   email TEXT
 );
--- src/main/resources/sql/populateCustomerTable.sql
+++ src/main/resources/sql/populateCustomerTable.sql
@@ -1,5 +1,5 @@
 INSERT INTO
-  customer (firs_name, last_name, email)
+  customer (first_name, last_name, email)
 VALUES
   ('Andrii', 'Yarmolenko', 'andrii.yarmolenko@gmail.com'),
   ('Andrii', 'Shevchenko', 'andrii.shevchenko@gmail.com'),
