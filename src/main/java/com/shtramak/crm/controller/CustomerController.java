57e002014bb83542e94db63f2b2d050b82cf2365
shtramak <shtramak@gmail.com>
2018-09-18 07:57:41 +0300

    add saveCustomer logic to CustomerController

--- src/main/java/com/shtramak/crm/controller/CustomerController.java
+++ src/main/java/com/shtramak/crm/controller/CustomerController.java
@@ -2,13 +2,14 @@ package com.shtramak.crm.controller;
 
 import java.util.List;
 
-import com.shtramak.crm.dao.CustomerDao;
 import com.shtramak.crm.entity.Customer;
 import com.shtramak.crm.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -27,5 +28,18 @@ public class CustomerController {
         List<Customer> customers = customerService.getCustomers();
         model.addAttribute("customers", customers);
         return "list-customers";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        Customer customer = new Customer();
+        model.addAttribute("customer", customer);
+        return "customer-form";
+    }
+
+    @PostMapping("/saveCustomer")
+    public String saveCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.saveCustomer(customer);
+        return "redirect:/customer/list";
     }
 }
