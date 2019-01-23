9b71477e576cbcf833f6642f18b5d180972ab043
shtramak <shtramak@gmail.com>
2019-01-23 15:40:54 +0200

    update CustomerDaoImpl to use EntityManager from spring context

--- src/main/java/com/shtramak/crm/dao/CustomerDaoImpl.java
+++ src/main/java/com/shtramak/crm/dao/CustomerDaoImpl.java
@@ -1,48 +1,42 @@
 package com.shtramak.crm.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 import com.shtramak.crm.entity.Customer;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public class CustomerDaoImpl implements CustomerDao {
-    private SessionFactory sessionFactory;
-
-    @Autowired
-    public CustomerDaoImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Override
     public List<Customer> getCustomers() {
-        Session session = sessionFactory.getCurrentSession();
-        Query<Customer> query = session.createQuery("FROM Customer ORDER BY lastName", Customer.class);
-        List<Customer> customers = query.getResultList();
-        return customers;
+        return entityManager.createQuery("FROM Customer ORDER BY lastName", Customer.class).getResultList();
     }
 
     @Override
     public void saveCustomer(Customer customer) {
-        Session session = sessionFactory.getCurrentSession();
-        session.saveOrUpdate(customer);
+        if (customer.getId() != null) {
+            entityManager.merge(customer);
+        } else {
+            entityManager.persist(customer);
+        }
     }
 
     @Override
     public Customer getCustomerById(Long customerId) {
-        Session session = sessionFactory.getCurrentSession();
-        return session.get(Customer.class, customerId);
+        return entityManager.find(Customer.class, customerId);
     }
 
     @Override
     public void deleteCustomerById(Long customerId) {
-        Session session = sessionFactory.getCurrentSession();
-        Query query = session.createQuery("DELETE FROM Customer WHERE id=:customerId");
-        query.setParameter("customerId",customerId);
-        query.executeUpdate();
+        entityManager.createQuery("delete from Customer where id=:customerId")
+                .setParameter("customerId", customerId)
+                .executeUpdate();
     }
 }
